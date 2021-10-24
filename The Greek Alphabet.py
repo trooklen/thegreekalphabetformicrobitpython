ae4c46833cfd1b74736826541740d6ae4d4f94ea
Yerassimos Stavaris <77192405+yerassimos2009@users.noreply.github.com>
2021-10-24 17:14:46 +0300

    Add files via upload

---
+++ The Greek Alphabet.py
@@ -0,0 +1,149 @@
+from microbit import *
+
+display.scroll("The Greek Alphabet")
+sleep(2000)
+display.show(Image("00900:"
+                    "09090:"
+                    "99999:"
+                    "90009:"
+                    "90009"))  #Greek Letter: Α
+sleep(1000)
+display.show(Image("09990:"
+                    "09009:"
+                    "09990:"
+                    "09009:"
+                    "09990"))  #Greek Letter: Β
+sleep(1000)
+display.show(Image("09999:"
+                    "09000:"
+                    "09000:"
+                    "09000:"
+                    "09000"))  #Greek Letter: Γ
+sleep(1000)
+display.show(Image("00900:"
+                    "09090:"
+                    "90009:"
+                    "90009:"
+                    "99999"))  #Greek Letter: Δ
+sleep(1000)
+display.show(Image("09999:"
+                    "09000:"
+                    "09999:"
+                    "09000:"
+                    "09999"))  #Greek Letter: Ε
+sleep(1000)
+display.show(Image("99999:"
+                    "09000:"
+                    "00900:"
+                    "00090:"
+                    "99999"))  #Greek Letter: Ζ
+sleep(1000)
+display.show(Image("90009:"
+                    "90009:"
+                    "99999:"
+                    "90009:"
+                    "90009"))  #Greek Letter: Η
+sleep(1000)
+display.show(Image("09990:"
+                    "90009:"
+                    "99999:"
+                    "90009:"
+                    "09990"))  #Greek Letter: Θ
+sleep(1000)
+display.show(Image("99999:"
+                    "00900:"
+                    "00900:"
+                    "00900:"
+                    "99999"))  #Greek Letter: Ι
+sleep(1000)
+display.show(Image("09009:"
+                    "09090:"
+                    "09900:"
+                    "09090:"
+                    "09009"))  #Greek Letter: Κ
+sleep(1000)
+display.show(Image("00900:"
+                    "09090:"
+                    "90009:"
+                    "90009:"
+                    "90009"))  #Greek Letter: Λ
+sleep(1000)
+display.show(Image("90009:"
+                    "99099:"
+                    "90909:"
+                    "90009:"
+                    "90009"))  #Greek Letter: Μ
+sleep(1000)
+display.show(Image("90009:"
+                    "99009:"
+                    "90909:"
+                    "90099:"
+                    "90009"))  #Greek Letter: Ν
+sleep(1000)
+display.show(Image("99999:"
+                    "00000:"
+                    "09990:"
+                    "00000:"
+                    "99999"))  #Greek Letter: Ξ
+sleep(1000)
+display.show(Image("09990:"
+                    "90009:"
+                    "90009:"
+                    "90009:"
+                    "09990"))  #Greek Letter: 0
+sleep(1000)
+display.show(Image("99999:"
+                    "90009:"
+                    "90009:"
+                    "90009:"
+                    "90009"))  #Greek Letter: Π
+sleep(1000)
+display.show(Image("09990:"
+                    "09009:"
+                    "09990:"
+                    "09000:"
+                    "09000"))  #Greek Letter: Ρ
+sleep(1500)
+display.show(Image("09999:"
+                    "00900:"
+                    "00090:"
+                    "00900:"
+                    "09999"))  #Greek Letter: Σ
+sleep(1000)
+display.show(Image("99999:"
+                    "00900:"
+                    "00900:"
+                    "00900:"
+                    "00900"))  #Greek Letter: Τ
+sleep(1000)
+display.show(Image("90009:"
+                    "09090:"
+                    "00900:"
+                    "00900:"
+                    "00900"))  #Greek Letter: Υ
+sleep(1000)
+display.show(Image("09990:"
+                    "90909:"
+                    "09990:"
+                    "00900:"
+                    "00900"))  #Greek Letter: Φ
+sleep(1000)
+display.show(Image("90009:"
+                    "09090:"
+                    "00900:"
+                    "09090:"
+                    "90009"))  #Greek Letter: Χ
+sleep(1000)
+display.show(Image("90909:"
+                    "90909:"
+                    "99999:"
+                    "00900:"
+                    "00900"))  #Greek Letter: Ψ
+sleep(1000)
+display.show(Image("99999:"
+                    "90009:"
+                    "99999:"
+                    "00000:"
+                    "99999"))  #Greek Letter: Ω
+
+
